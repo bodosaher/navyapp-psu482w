c7865753539dca94ac4814385ceb9c37171e78ab
zpdeering <zpdeering@gmail.com>
2013-11-18 17:14:32 -0500

    Reworked GUI

--- NavyApp1/Assets/Camera1.js
+++ NavyApp1/Assets/Camera1.js
@@ -4,6 +4,9 @@ var cam1 : Camera;
 var cam2 : Camera;
 var cam3 : Camera;
 var cam4 : Camera;
+var GUIEnabled : boolean = false;
+var settingsIcon : Texture2D;
+var customGUISkin : GUISkin;
  
 function Start() 
 {
@@ -15,75 +18,46 @@ function Start()
 
 // JavaScript
 function OnGUI () {
-	// Make a background box
-	GUI.Box (Rect (10,10,110,150), "Camera Select");
 
-	// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-	if (GUI.Button (Rect (20,40,90,20), "Main Camera")) {
-		cam1.enabled = true;
-		cam2.enabled = false;
-		cam3.enabled = false;
-		cam4.enabled = false;
+	GUI.skin = customGUISkin;
+	if(GUI.Button(Rect(Screen.width - 50,10,40,40), GUIContent(settingsIcon))) {
+		GUIEnabled = !GUIEnabled;
 	}
+	GUI.skin = null;
+	if(GUIEnabled) {
+		// Make a background box
+		GUI.Box (Rect (10,10,610,50), "");
 
-	// Make the second button.
-	if (GUI.Button (Rect (20,70,90,20), "Bow View")) {
-		cam1.enabled = false;
-		cam2.enabled = true;
-		cam3.enabled = false;
-		cam4.enabled = false;
-	}
-	
-	// Make the third button.
-	if (GUI.Button (Rect (20,100,90,20), "Side View")) {
-		cam1.enabled = false;
-		cam2.enabled = false;
-		cam3.enabled = true;
-		cam4.enabled = false;
-	}
-	
-	// Make the fourth button.
-	if (GUI.Button (Rect (20,130,90,20), "Top View")) {
-		cam1.enabled = false;
-		cam2.enabled = false;
-		cam3.enabled = false;
-		cam4.enabled = true;
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if (GUI.Button (Rect (30,15,120,40), "Main Camera")) {
+			cam1.enabled = true;
+			cam2.enabled = false;
+			cam3.enabled = false;
+			cam4.enabled = false;
+		}
+
+		// Make the second button.
+		if (GUI.Button (Rect (180,15,120,40), "Bow View")) {
+			cam1.enabled = false;
+			cam2.enabled = true;
+			cam3.enabled = false;
+			cam4.enabled = false;
+		}
+		
+		// Make the third button.
+		if (GUI.Button (Rect (330,15,120,40), "Side View")) {
+			cam1.enabled = false;
+			cam2.enabled = false;
+			cam3.enabled = true;
+			cam4.enabled = false;
+		}
+		
+		// Make the fourth button.
+		if (GUI.Button (Rect (480,15,120,40), "Top View")) {
+			cam1.enabled = false;
+			cam2.enabled = false;
+			cam3.enabled = false;
+			cam4.enabled = true;
+		}
 	}
 }
-/*
-function Update() 
-{ 
-	if (Input.GetKeyDown(KeyCode.F1)) 
-	{
-		cam1.enabled = true;
-		cam2.enabled = false;
-		cam3.enabled = false;
-		cam4.enabled = false;
-	}
-	
-	if (Input.GetKeyDown(KeyCode.F2)) 
-	{
-		cam1.enabled = false;
-		cam2.enabled = true;
-		cam3.enabled = false;
-		cam4.enabled = false;
-	}
-	
-	if (Input.GetKeyDown(KeyCode.F3)) 
-	{
-		cam1.enabled = false;
-		cam2.enabled = false;
-		cam3.enabled = true;
-		cam4.enabled = false;
-	}
-	
-	if (Input.GetKeyDown(KeyCode.F4)) 
-	{
-		cam1.enabled = false;
-		cam2.enabled = false;
-		cam3.enabled = false;
-		cam4.enabled = true;
-	}
-	
-}
-*/
