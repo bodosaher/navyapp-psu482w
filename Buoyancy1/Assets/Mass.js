d03f6541cffe9b603854de9de9f4397cf1aaae38
othy001 <othy001@gmail.com>
2013-12-03 14:05:39 -0500

    New Buoyancy
    Added buoyancy1 project, which includes all the new buoyancy stuff, the
    ship now stays afloat, I used a box collider instead of mesh since our
    boat's mesh isn't really working.  Feel free to mess with the box and
    such to get a better simulation.  The account for unity is:
    User: NavyApp13@gmail.com
    Pass: ShipMotion2013

---
+++ Buoyancy1/Assets/Mass.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+	//rigidbody.centerOfMass = Vector3 (0, 0, 0);
+	//rigidbody.centerOfMass = Vector3 (-5.879, -1.25766, -4);
+}
+
+function Update () {
+
+//rigidbody.centerOfMass = Vector3 (0, -60, 0);
+
+}
